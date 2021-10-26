3aadd089badfd13dfacbb253521e4121ce110c95
Artur Oczkowski <alfaix@o2.pl>
2021-10-26 23:31:16 +0200

    1_4 Wyświetlanie plików z katalogu z LINQ (query syntax)

--- LINQ/1 Wprowadzenie/Program.cs
+++ LINQ/1 Wprowadzenie/Program.cs
@@ -13,7 +13,21 @@ namespace _1_Wprowadzenie
         {
             var sciezka = @"c:\windows";
             PokazDuzePlikiBezLinq(sciezka);
+            Console.WriteLine("*******************");
+            PokazDuzePlikiZLinq(sciezka);
             
+        }
+
+        private static void PokazDuzePlikiZLinq(string sciezka)
+        {
+            var zapytanie = from plik in new DirectoryInfo(sciezka).GetFiles()
+                            orderby plik.Length descending
+                            select plik;
+
+            foreach (var plik in zapytanie.Take(5))
+            {
+                Console.WriteLine($"{plik.Name,-20} : {plik.Length / 1024,20:N0}KB");
+            }
         }
 
         private static void PokazDuzePlikiBezLinq(string sciezka)
