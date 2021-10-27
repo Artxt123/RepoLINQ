ec4a778b4667ea88faf1027f9af2598d1a1ebc9d
Artur Oczkowski <alfaix@o2.pl>
2021-10-27 22:27:22 +0200

    2_2 Siła IEnumerable

---
+++ LINQ/2 Funkcje/Pracownik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Funkcje
+{
+    public class Pracownik
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ LINQ/2 Funkcje/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Pracownik> programisci = new Pracownik[]
+            {
+                new Pracownik{Id = 1, Imie = "Marek", Nazwisko = "Rajkiewicz"},
+                new Pracownik{Id = 2, Imie = "Wojciech", Nazwisko = "Wiertlewski"},
+                new Pracownik{Id = 3, Imie = "Piotr", Nazwisko = "Lis"},
+                new Pracownik{Id = 4, Imie = "Wojciech", Nazwisko = "Karbowiak"}
+            };
+
+            IEnumerable<Pracownik> kierowcy = new List<Pracownik>()
+            {
+                new Pracownik{Id = 5, Imie = "Michał", Nazwisko = "Andrzejak"},
+                new Pracownik{Id = 6, Imie = "Marcin", Nazwisko = "Poradzisz"},
+                new Pracownik{Id = 7, Imie = "Tomasz", Nazwisko = "Nowak"}
+            };
+            //Dzięki temu, że tablica i lista mogą korzystać z IEnumerable<T>, możemy iterować po tych kolekcjach
+            foreach (var osoba in programisci)
+            {
+                Console.WriteLine($"{osoba.Imie} {osoba.Nazwisko}");
+            }
+
+            //Teraz zrobimy to samo co w pętli foreach, tylko krok po kroku:
+
+            IEnumerator<Pracownik> enumerator = kierowcy.GetEnumerator();
+
+            while (enumerator.MoveNext()) //MoveNext() zwraca true, gdy można przejść do nast. elementu; false gdy nie można
+            {
+                Console.WriteLine($"{enumerator.Current.Imie} {enumerator.Current.Nazwisko}");
+            }
+        }
+    }
+}
