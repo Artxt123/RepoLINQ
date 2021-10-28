e5ed1a26eb7f8ec671ca441dd1d9ec6e5d9e436e
Artur Oczkowski <alfaix@o2.pl>
2021-10-28 23:47:24 +0200

    3_2 Tworzenie własnego filtra

---
+++ LINQ/3 Zapytania/NaszLinq.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Zapytania
+{
+    public static class NaszLinq
+    {
+        public static IEnumerable<T> Filtr<T>(this IEnumerable<T> zrodlo, Func<T, bool> predicate)
+        {
+            var wynik = new List<T>();
+
+            foreach (var item in zrodlo)
+            {
+                if (predicate(item))
+                {
+                    wynik.Add(item);
+                }
+            }
+
+            return wynik;
+        }
+    }
+}
