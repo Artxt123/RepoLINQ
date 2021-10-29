e8a9bb9ea2feaf07fdf60ecd848fd8f0e7288183
Artur Oczkowski <alfaix@o2.pl>
2021-10-29 22:53:53 +0200

    3_6 Unikanie pułapek odroczonego wykonania

--- LINQ/3 Zapytania/Program.cs
+++ LINQ/3 Zapytania/Program.cs
@@ -20,7 +20,9 @@ namespace _3_Zapytania
                 new Film{Tytul = "Gladiator", Gatunek = "Dramat", Ocena = 8.1f, Rok = 2000}
             };
 
-            var zapytanie = filmy.Filtr(f => f.Rok > 2002);
+            var zapytanie = filmy.Filtr(f => f.Rok > 2002).ToList();
+
+            Console.WriteLine(zapytanie.Count());
 
             var enumerator = zapytanie.GetEnumerator();
 
