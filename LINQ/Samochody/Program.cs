0ede131a619f7711599df911a63245fb5b51ccdb
Artur Oczkowski <alfaix@o2.pl>
2021-10-31 00:04:27 +0200

    4_3 Implementacja wczytywania pliku

--- LINQ/Samochody/Program.cs
+++ LINQ/Samochody/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,28 @@ namespace Samochody
     {
         static void Main(string[] args)
         {
+            #region OperacjeNaCultureInfo
+            //Gdybym chciał wyświetlać w konsoli kropkę zamiast przecinka dla liczb np. double
+            //var newCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            //newCulture.NumberFormat.NumberDecimalSeparator = ".";
+            //CultureInfo.CurrentCulture = newCulture;
+            #endregion
+
+            var samochody = WczytywaniePliku("paliwo.csv");
+
+            foreach (var samochod in samochody)
+            {
+                Console.WriteLine($"{samochod.Producent} {samochod.Model}");
+            }
+        }
+
+        private static List<Samochod> WczytywaniePliku(string sciezka)
+        {
+            return File.ReadAllLines(sciezka)
+                       .Skip(1) //pomijamy pierwszą linię z pliku csv, która jest nagłówkiem
+                       .Where(linia => linia.Length > 1) //filtrujemy i odrzucamy linie, które nie mają w sobie danych, (przy założeniu, że jak linia ma 1 lub 0 znaków, to ta linia nie ma danych)
+                       .Select(Samochod.ParsujCSV) //przekształcamy pobrane linie na obiekty typu Samochod, dzięki metodzie ParsujCSV
+                       .ToList(); //Zamieniamy pobrane dane na listę samochodów
         }
     }
 }
--- LINQ/Samochody/Samochod.cs
+++ LINQ/Samochody/Samochod.cs
@@ -1,4 +1,6 @@
-﻿namespace Samochody
+﻿using System;
+using System.Globalization;
+namespace Samochody
 {
     public class Samochod
     {
@@ -10,5 +12,25 @@
         public int SpalanieMiasto { get; set; }
         public int SpalanieAutostrada { get; set; }
         public int SpalanieMieszane { get; set; }
+
+        internal static Samochod ParsujCSV(string linia)
+        {
+            var kolumny = linia.Split(','); //dzielimy każdą linię na kolumny; przy założeniu, że każda kolumna oddzielona jest od następnej przecinkiem
+
+            //Potrzebuję tego, bo inaczej dane nie chcą przejść; u mnie w csv liczby double musiałby mieć przecinek, a tam są kropki
+            double.TryParse(kolumny[3], NumberStyles.Any, CultureInfo.InvariantCulture, out double pojemnosc);
+
+            return new Samochod
+            {
+                Rok = int.Parse(kolumny[0]),
+                Producent = kolumny[1],
+                Model = kolumny[2],
+                Pojemnosc = pojemnosc,
+                IloscCylindrow = int.Parse(kolumny[4]),
+                SpalanieMiasto = int.Parse(kolumny[5]),
+                SpalanieAutostrada = int.Parse(kolumny[6]),
+                SpalanieMieszane = int.Parse(kolumny[7])
+            };
+        }
     }
 }
